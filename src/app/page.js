4c2b04d9a9879e4c1e3f4bf5def7813f1ad92624
86156 <3309994926@qq.com>
2025-03-20 21:55:02 +0800

    第一次分区

--- src/app/page.js
+++ src/app/page.js
@@ -4,25 +4,33 @@
 
 /* 导入 Next.js 的图片组件，优化图像加载和显示 */
 import Image from "next/image";
-
+import { Card } from "@/components/ui/card";
+import MainCard from "@/components/datacard";
 /* 从 components 目录导入自定义的 Button 组件 */
 import { Button } from "@/components/ui/button";
 
 /* 定义主页组件 */
 export default function Home() {
   return (
-    /* 使用 Tailwind 的 flex 布局，使内容垂直居中 */
-    <div className="flex flex-col items-center justify-center min-h-screen p-8">
-      
-      {/* 显示标题文字，text-xl 设置文字大小为 XL，font-bold 设置加粗 */}
-      <h1 className="text-xl font-bold mt-4">Welcome to Next.js!</h1>
-
+    <div >
+    <header className="flex flex-col items-center justify-center">
+      {/* 显示标题文字，text-xl 设置文字大小为 XL，font-bold 设置加粗*/}
+      <h1 className="text-xl font-bold mt-4">欢迎来到城建心语</h1>
       {/* 使用灰色文字提示用户编辑代码，text-gray-500 控制颜色 */}
-      <p className="text-gray-500">Get started by editing <code>src/app/page.js</code>.</p>
-
+      {/* <p className="text-gray-500">Get started by editing <code>src/app/page.js</code>.</p> */}
       {/* 使用导入的 Button 组件，variant="outline" 表示边框按钮 */}
       <Button variant="outline">Button</Button>
-
+    </header> 
+    {/* 给区块分区 */}
+    <div className="flex flex-row">
+      <div className="w-300 h-200 bg-gray-200 mb-4"></div>
+      {/* 第二、第三个 div，占据38%的宽度和20%的高度 */}
+      <div className="flex flex-col">
+        <div className="w-120 h-100 bg-gray-300"></div>
+        <div className="w-120 h-100 bg-gray-400"></div>
+      </div>
     </div>
+    </div>
+
   );
 }
---
+++ src/components/datacard.jsx
@@ -0,0 +1,18 @@
+//导入card
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  export default function DataCard() {
+    return (
+     
+       <Card></Card>
+     
+    );
+  }
+  
+  
